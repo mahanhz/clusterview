dc76e8cb720a1b8808d73d7be07228a495cff19d
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-11-17 14:53:03 +0100

    Added ActivityTest

---
+++ src/test/java/org/amhzing/clusterview/domain/model/ActivityTest.java
@@ -0,0 +1,18 @@
+package org.amhzing.clusterview.domain.model;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ActivityTest {
+
+    @Test
+    public void test_equals_hashcode() {
+        final Activity activity = Activity.STUDY_CIRCLE;
+        final Activity activity2 = Activity.valueOf("STUDY_CIRCLE");
+        final Activity activity3 = Activity.CHILDRENS_CLASS;
+
+        assertThat(activity).isEqualTo(activity2);
+        assertThat(activity).isNotEqualTo(activity3);
+    }
+}
