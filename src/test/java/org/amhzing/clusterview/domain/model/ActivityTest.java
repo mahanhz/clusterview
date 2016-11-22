37dcd1b8c8e991d46edeeba28e5ceb32c1c239ea
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-11-22 20:14:53 +0100

    Fixed test

--- src/test/java/org/amhzing/clusterview/domain/model/ActivityTest.java
+++ src/test/java/org/amhzing/clusterview/domain/model/ActivityTest.java
@@ -3,14 +3,8 @@ package org.amhzing.clusterview.domain.model;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.Test;
-import org.meanbean.test.BeanTester;
 
 public class ActivityTest {
-
-    @Test
-    public void getterAndSetterCorrectness() throws Exception {
-        new BeanTester().testBean(Activity.class);
-    }
 
     @Test
     public void equalsAndHashCodeContract() throws Exception {
