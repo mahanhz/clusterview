337cb84c7c7bfc2c285e66e2c8518c1543868922
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-11-22 22:44:55 +0100

    Added group controller test

---
+++ src/test/java/org/amhzing/clusterview/web/controller/se/GroupControllerTest.java
@@ -0,0 +1,65 @@
+package org.amhzing.clusterview.web.controller.se;
+
+import com.google.common.collect.ImmutableSet;
+import org.amhzing.clusterview.annotation.TestOffline;
+import org.amhzing.clusterview.web.adapter.GroupAdapter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.amhzing.clusterview.helper.ClientModelHelper.groupModel;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GroupController.class)
+@TestOffline
+public class GroupControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private GroupAdapter groupAdapter;
+
+    @Test
+    public void should_get_groups() throws Exception {
+        given(groupAdapter.groups(any())).willReturn(ImmutableSet.of(groupModel()));
+
+        final ResultActions result = this.mvc.perform(get("/clusterview/se/central/stockholm"))
+                                             .andExpect(status().isOk());
+
+        final String content = result.andReturn().getResponse().getContentAsString();
+
+        Document doc = Jsoup.parse(content);
+        final Element cluster = doc.getElementById("cluster");
+
+        assertThat(cluster.val()).isEqualToIgnoringCase("stockholm");
+    }
+
+    @Test
+    public void should_get_group() throws Exception {
+        given(groupAdapter.group(1L)).willReturn(groupModel());
+
+        final ResultActions result = this.mvc.perform(get("/clusterview/se/central/stockholm/1"))
+                                             .andExpect(status().isOk());
+
+        final String content = result.andReturn().getResponse().getContentAsString();
+
+        Document doc = Jsoup.parse(content);
+        final Element groupId = doc.getElementById("groupId");
+
+        assertThat(groupId.val()).isEqualToIgnoringCase("1");
+    }
+}
