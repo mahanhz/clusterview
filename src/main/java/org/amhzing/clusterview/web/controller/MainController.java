594212a06824bd20cc1f170b69f3ac1cf919fc42
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-11-22 22:53:44 +0100

    Added tests for other controllers

--- src/main/java/org/amhzing/clusterview/web/controller/MainController.java
+++ src/main/java/org/amhzing/clusterview/web/controller/MainController.java
@@ -2,6 +2,7 @@ package org.amhzing.clusterview.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class MainController extends AbstractController {
 
     @GetMapping(path = "/{country}")
-    public ModelAndView country(@PathVariable final String country) {
+    public ModelAndView country(@ModelAttribute @PathVariable final String country) {
         return new ModelAndView(country + "/index");
     }
 }
---
+++ src/test/java/org/amhzing/clusterview/web/controller/MainControllerTest.java
@@ -0,0 +1,39 @@
+package org.amhzing.clusterview.web.controller;
+
+import org.amhzing.clusterview.annotation.TestOffline;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainController.class)
+@TestOffline
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void should_get_country() throws Exception {
+        final ResultActions result = this.mvc.perform(get("/clusterview/se"))
+                                             .andExpect(status().isOk());
+
+        final String content = result.andReturn().getResponse().getContentAsString();
+
+        Document doc = Jsoup.parse(content);
+        final Element country = doc.getElementById("country");
+
+        assertThat(country.val()).isEqualToIgnoringCase("se");
+    }
+}
