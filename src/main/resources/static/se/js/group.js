d7abf7a9c363fc7f36f26e6e5c75ff8721096788
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-11-28 15:28:36 +0100

    Added unit tests for controllers

--- src/main/resources/static/se/js/group.js
+++ src/main/resources/static/se/js/group.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
 
-    $("#add_row").click(function() {
-        var i = parseInt($('input[type="checkbox"][name="memberIds"]').length);
-        var options = $("#activityOptions").html();
-        $('#member'+i).html('<td><input type="checkbox" name="memberIds" /></td><td><input type="text" name="members[' + i + '].name.firstName"  placeholder="First name" class="form-control"/></td><td><input type="text" name="members[' + i + '].name.lastName" placeholder="Last name" class="form-control"/></td><td><select class="activitiesMultiSelect" name="members[' + i + '].capability.activities" multiple="multiple">' + options + '</select></td><td><select class="activitiesMultiSelect" name="members[' + i + '].commitment.activities" multiple="multiple">' + options + '</select></td>');
+    $('.activitiesMultiSelect').multiselect({
+        numberDisplayed: 1,
+        buttonWidth: '250px'
+    });
+});
 
-        $('.activitiesMultiSelect').multiselect({
-            numberDisplayed: 1,
-            buttonWidth: '250px'
-        });
+$("#add_row").click(function() {
+    var i = parseInt($('input[type="checkbox"][name="memberIds"]').length);
+    var options = $("#activityOptions").html();
+    $('#member' + i).html('<td><input type="checkbox" name="memberIds" /></td><td><input type="text" name="members[' + i + '].name.firstName"  placeholder="First name" class="form-control"/></td><td><input type="text" name="members[' + i + '].name.lastName" placeholder="Last name" class="form-control"/></td><td><select class="activitiesMultiSelect" name="members[' + i + '].capability.activities" multiple="multiple">' + options + '</select></td><td><select class="activitiesMultiSelect" name="members[' + i + '].commitment.activities" multiple="multiple">' + options + '</select></td>');
 
-        $('#membersTable').append('<tr id="member'+(i+1)+'"></tr>');
-        i++;
+    $('.activitiesMultiSelect').multiselect({
+        numberDisplayed: 1,
+        buttonWidth: '250px'
     });
 
-    $("#delete_row").click(function() {
-        $('input[type="checkbox"][name="memberIds"]:checked').each(function() {
+    $('#membersTable').append('<tr id="member'+(i+1)+'"></tr>');
+});
+
+$("#delete_row").click(function() {
+    var selectedMembers = $('input[type="checkbox"][name="memberIds"]:checked');
+    var numSelected = selectedMembers.length;
+
+    if (numSelected > 0) {
+        selectedMembers.each(function() {
             var member = $(this).closest('tr').remove();
         });
 
@@ -34,12 +43,7 @@ $(document).ready(function() {
 
             index++;
         });
-	});
-
-    $('.activitiesMultiSelect').multiselect({
-        numberDisplayed: 1,
-        buttonWidth: '250px'
-    });
+    }
 });
 
 $('.map').click(function(e) {
---
+++ src/test/java/org/amhzing/clusterview/web/controller/se/GroupEditControllerTest.java
@@ -0,0 +1,82 @@
+package org.amhzing.clusterview.web.controller.se;
+
+import com.google.common.collect.ImmutableSet;
+import org.amhzing.clusterview.annotation.TestOffline;
+import org.amhzing.clusterview.web.adapter.ActivityAdapter;
+import org.amhzing.clusterview.web.adapter.GroupAdapter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.amhzing.clusterview.helper.ClientModelHelper.groupModel;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GroupEditController.class)
+@TestOffline
+public class GroupEditControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private GroupAdapter groupAdapter;
+
+    @MockBean
+    private ActivityAdapter activityAdapter;
+
+    @Test
+    public void should_be_able_to_create_new_group() throws Exception {
+        given(groupAdapter.groups(any())).willReturn(ImmutableSet.of(groupModel()));
+
+        final ResultActions result = this.mvc.perform(get("/clusteredit/se/central/stockholm/newgroup"))
+                                             .andExpect(status().isOk());
+
+        final String content = result.andReturn().getResponse().getContentAsString();
+
+        Document doc = Jsoup.parse(content);
+        final Elements forms = doc.getElementsByTag("form");
+
+        assertThat(forms).hasSize(1);
+        final Element form = forms.get(0);
+
+        assertThat(form.attr("action")).isEqualTo("creategroup");
+        assertThat(form.attr("method")).isEqualToIgnoringCase("post");
+    }
+
+    @Test
+    public void sshould_be_able_to_edit_new_group() throws Exception {
+        given(groupAdapter.group(1L)).willReturn(groupModel());
+
+        final ResultActions result = this.mvc.perform(get("/clusteredit/se/central/stockholm/1"))
+                                             .andExpect(status().isOk());
+
+        final String content = result.andReturn().getResponse().getContentAsString();
+
+        Document doc = Jsoup.parse(content);
+        final Elements forms = doc.getElementsByTag("form");
+
+        assertThat(forms).hasSize(1);
+        final Element form = forms.get(0);
+
+        assertThat(form.attr("action")).isEqualTo("1");
+        assertThat(form.attr("method")).isEqualToIgnoringCase("post");
+
+        final Elements hiddenMethodElements = form.getElementsByAttributeValueMatching("name", "_method");
+        assertThat(hiddenMethodElements).hasSize(1);
+        assertThat(hiddenMethodElements.get(0).attr("value")).isEqualToIgnoringCase("put");
+    }
+}
