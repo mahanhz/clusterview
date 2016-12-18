c602a62d68dd14f15851b3854d62ac4872f9d783
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-12-18 15:08:28 +0100

    Added web security tests

--- src/test/java/org/amhzing/clusterview/configuration/handler/RedirectAuthenticationSuccessHandlerTest.java
+++ src/test/java/org/amhzing/clusterview/configuration/handler/RedirectAuthenticationSuccessHandlerTest.java
@@ -1,30 +1,22 @@
 package org.amhzing.clusterview.configuration.handler;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import org.amhzing.clusterview.domain.model.Country;
-import org.amhzing.clusterview.user.DefaultUserDetails;
-import org.amhzing.clusterview.user.UserRole;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
-import java.util.List;
 
 import static org.amhzing.clusterview.configuration.handler.RedirectAuthenticationSuccessHandler.URI;
+import static org.amhzing.clusterview.helper.AuthenticationHelper.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RedirectAuthenticationSuccessHandlerTest {
 
-    private static final String COUNTRY = "se";
     private final RedirectAuthenticationSuccessHandler handler = new RedirectAuthenticationSuccessHandler();
 
     @Mock
@@ -41,22 +33,6 @@ public class RedirectAuthenticationSuccessHandlerTest {
 
         handler.onAuthenticationSuccess(request, response, authentication);
 
-        assertThat(handler.uri(authentication)).isEqualTo(URI + COUNTRY);
-    }
-
-    private DefaultUserDetails userDetails() {
-        return new DefaultUserDetails("test", "p", true, true, true, true,
-                                      role(),
-                                      "First",
-                                      "Last",
-                                      countries());
-    }
-
-    private Collection role() {
-        return ImmutableSet.of(new SimpleGrantedAuthority(UserRole.USER.getRole()));
-    }
-
-    private List<Country.Id> countries() {
-        return ImmutableList.of(Country.Id.create(COUNTRY));
+        assertThat(handler.uri(authentication)).isEqualTo(URI + AUTHENTICATION_COUNTRY);
     }
 }
---
+++ src/test/java/org/amhzing/clusterview/user/WebSecurityTest.java
@@ -0,0 +1,63 @@
+package org.amhzing.clusterview.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.core.Authentication;
+
+import static org.amhzing.clusterview.helper.AuthenticationHelper.AUTHENTICATION_COUNTRY;
+import static org.amhzing.clusterview.helper.AuthenticationHelper.userDetails;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WebSecurityTest {
+
+    @Mock
+    private Authentication authentication;
+
+    private WebSecurity webSecurity;
+
+    @Before
+    public void setUp() throws Exception {
+        webSecurity = new WebSecurity();
+    }
+
+    @Test
+    public void should_allow_when_user_belongs_to_country() throws Exception {
+        given(authentication.getPrincipal()).willReturn(userDetails());
+
+        final boolean result = webSecurity.checkCountry(authentication, AUTHENTICATION_COUNTRY);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void should_not_allow_when_user_does_not_belong_to_country() throws Exception {
+        given(authentication.getPrincipal()).willReturn(userDetails());
+
+        final boolean result = webSecurity.checkCountry(authentication, "dk");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void should_allow_when_user_belongs_to_cluster() throws Exception {
+        given(authentication.getPrincipal()).willReturn(userDetails());
+
+        final boolean result = webSecurity.checkAdmin(authentication, "stockholm");
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void should_not_allow_when_user_does_not_belong_to_cluster() throws Exception {
+        given(authentication.getPrincipal()).willReturn(userDetails());
+
+        final boolean result = webSecurity.checkAdmin(authentication, "uppsala");
+
+        assertThat(result).isFalse();
+    }
+}
