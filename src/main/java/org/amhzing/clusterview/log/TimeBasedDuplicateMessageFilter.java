85cf2ed3fbd274c4127d7f94c30062ce02729cb8
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-12-19 20:51:19 +0100

    Added a time based duplicate log message filter

---
+++ src/main/java/org/amhzing/clusterview/log/TimeBasedDuplicateMessageFilter.java
@@ -0,0 +1,104 @@
+package org.amhzing.clusterview.log;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.turbo.TurboFilter;
+import ch.qos.logback.core.spi.FilterReply;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.slf4j.Marker;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+public class TimeBasedDuplicateMessageFilter extends TurboFilter {
+
+    public static final int DEFAULT_CACHE_SIZE = 100;
+
+    public static final int DEFAULT_ALLOWED_REPETITIONS = 5;
+
+    public int allowedRepetitions = DEFAULT_ALLOWED_REPETITIONS;
+    public int cacheSize = DEFAULT_CACHE_SIZE;
+    public int expireAfterWriteSeconds = 60;
+
+    private Cache<String, Integer> msgCache;
+
+    @Override
+    public void start() {
+
+        msgCache = buildCache();
+
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        msgCache.invalidateAll();
+        msgCache = null;
+
+        super.stop();
+    }
+
+    @Override
+    public FilterReply decide(final Marker marker, final Logger logger, final Level level,
+                              final String format, final Object[] params, final Throwable t) {
+        int count = 0;
+
+        if (isNotBlank(format)) {
+            final String key = format + paramsAsString(params);
+
+            final Integer msgCount = msgCache.getIfPresent(key);
+
+            if (msgCount != null) {
+                count = msgCount + 1;
+            }
+
+            msgCache.put(key, count);
+        }
+
+        return (count <= allowedRepetitions) ? FilterReply.NEUTRAL : FilterReply.DENY;
+    }
+
+    public int getAllowedRepetitions() {
+        return allowedRepetitions;
+    }
+
+    public void setAllowedRepetitions(final int allowedRepetitions) {
+        this.allowedRepetitions = allowedRepetitions;
+    }
+
+    public int getCacheSize() {
+        return cacheSize;
+    }
+
+    public void setCacheSize(final int cacheSize) {
+        this.cacheSize = cacheSize;
+    }
+
+    public int getExpireAfterWriteSeconds() {
+        return expireAfterWriteSeconds;
+    }
+
+    public void setExpireAfterWriteSeconds(final int expireAfterWriteSeconds) {
+        this.expireAfterWriteSeconds = expireAfterWriteSeconds;
+    }
+
+    private String paramsAsString(final Object[] params) {
+        if (params != null) {
+            return Arrays.stream(params).map(Object::toString).collect(Collectors.joining("_"));
+        }
+
+        return "";
+    }
+
+    private Cache<String, Integer> buildCache() {
+        return CacheBuilder.newBuilder()
+                           .expireAfterWrite(expireAfterWriteSeconds, TimeUnit.SECONDS)
+                           .initialCapacity(cacheSize)
+                           .maximumSize(cacheSize)
+                           .build();
+    }
+}
---
+++ src/test/java/org/amhzing/clusterview/log/TimeBasedDuplicateMessageFilterTest.java
@@ -0,0 +1,74 @@
+package org.amhzing.clusterview.log;
+
+import ch.qos.logback.core.spi.FilterReply;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TimeBasedDuplicateMessageFilterTest {
+
+    @Test
+    public void should_not_allow_repetitions() {
+        final TimeBasedDuplicateMessageFilter dmf = new TimeBasedDuplicateMessageFilter();
+        dmf.setAllowedRepetitions(0);
+        dmf.start();
+
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.NEUTRAL);
+
+        // repetition
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.DENY);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.DENY);
+    }
+
+    @Test
+    public void should_allow_one_repetition() {
+        final TimeBasedDuplicateMessageFilter dmf = new TimeBasedDuplicateMessageFilter();
+        dmf.setAllowedRepetitions(1);
+        dmf.start();
+
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.NEUTRAL);
+
+        // repetition 1
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.NEUTRAL);
+
+        // repetition 2
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.DENY);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.DENY);
+    }
+
+    @Test
+    public void should_allow_null_repetitions() {
+        final TimeBasedDuplicateMessageFilter dmf = new TimeBasedDuplicateMessageFilter();
+        dmf.setAllowedRepetitions(0);
+        dmf.start();
+
+        assertThat(logMessage(dmf, null)).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, null)).isEqualTo(FilterReply.NEUTRAL);
+    }
+
+    @Test
+    public void should_allow_repetitions_when_cache_is_too_small() {
+        final TimeBasedDuplicateMessageFilter dmf = new TimeBasedDuplicateMessageFilter();
+        dmf.setAllowedRepetitions(1);
+        dmf.setCacheSize(1);
+        dmf.start();
+
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.NEUTRAL);
+
+        // repetition 1
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.NEUTRAL);
+
+        // repetition 2
+        assertThat(logMessage(dmf, "x")).isEqualTo(FilterReply.NEUTRAL);
+        assertThat(logMessage(dmf, "y")).isEqualTo(FilterReply.NEUTRAL);
+    }
+
+    private FilterReply logMessage(final TimeBasedDuplicateMessageFilter dmf, final String message) {
+        return dmf.decide(null, null, null, message, null, null);
+    }
+}
