3f49fdf8eb0c68e8b487a6905b580fc49929e97c
Mahan Hashemizadeh <mahanhz@gmail.com>
2017-01-11 15:49:20 +0100

    Fixed unit test

--- src/test/java/org/amhzing/clusterview/domain/model/LocationTest.java
+++ src/test/java/org/amhzing/clusterview/domain/model/LocationTest.java
@@ -1,15 +1,20 @@
 package org.amhzing.clusterview.domain.model;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.amhzing.clusterview.helper.JUnitParamHelper.valid;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@RunWith(JUnitParamsRunner.class)
 public class LocationTest {
 
-
+    @Test
+    @Parameters(method = "values")
     public void test_creation(final Class<? extends Exception> exception,
                               final int x,
                               final int y) {
@@ -20,9 +25,9 @@ public class LocationTest {
         }
     }
 
-
+    @Test
     public void equalsAndHashCodeContract() throws Exception {
-        EqualsVerifier.forClass(Location.class).suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS).verify();
+        EqualsVerifier.forClass(ImmutableLocation.class).suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS).verify();
     }
 
     @SuppressWarnings("unused")
