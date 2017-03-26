917cd94a9caa1f44dd2c7f50b48c2fba4fbf3233
Mahan Hashemizadeh <mahanhz@gmail.com>
2017-03-26 21:59:57 +0200

    Added test for group edit rest controller

---
+++ app/src/test/java/org/amhzing/clusterview/app/web/controller/rest/appnav/GroupEditRestControllerTest.java
@@ -0,0 +1,97 @@
+package org.amhzing.clusterview.app.web.controller.rest.appnav;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.amhzing.clusterview.app.annotation.TestOffline;
+import org.amhzing.clusterview.app.api.GroupDTO;
+import org.amhzing.clusterview.app.application.GroupService;
+import org.amhzing.clusterview.app.security.WithMockCustomUser;
+import org.amhzing.clusterview.app.web.MediaTypes;
+import org.amhzing.clusterview.app.web.adapter.Obfuscator;
+import org.amhzing.clusterview.app.web.controller.rest.util.GroupDtoFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.amhzing.clusterview.app.helper.DomainModelHelper.group;
+import static org.amhzing.clusterview.app.web.controller.rest.RestControllerPath.BASE_PATH;
+import static org.amhzing.clusterview.app.web.model.GroupPath.CREATE_GROUP;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GroupEditRestController.class)
+@TestOffline
+public class GroupEditRestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private GroupService groupService;
+
+    @Test
+    @WithMockCustomUser(username = "testU", password = "NotSaying")
+    public void should_create_group() throws Exception {
+
+        final ObjectMapper mapper = new ObjectMapper();
+        final GroupDTO groupDto = GroupDtoFactory.convertGroup(group());
+
+        final String json = mapper.writeValueAsString(groupDto);
+
+        final String urlSegment = "/se/central/cluster1";
+        final ResultActions result = mvc.perform(post(BASE_PATH + "/clusteredit" + urlSegment + "/" + CREATE_GROUP)
+                                                         .contentType(MediaTypes.APPLICATION_JSON_V1)
+                                                         .content(json))
+                                        .andExpect(status().isCreated());
+
+        final String locationHeader = result.andReturn().getResponse().getHeader("Location");
+
+        assertThat(locationHeader).isNotBlank();
+        assertThat(locationHeader).endsWith(BASE_PATH + "/clusterview" + urlSegment);
+    }
+
+    @Test
+    @WithMockCustomUser(username = "testU", password = "NotSaying")
+    public void should_update_group() throws Exception {
+
+        final ObjectMapper mapper = new ObjectMapper();
+        final GroupDTO groupDto = GroupDtoFactory.convertGroup(group());
+
+        final String json = mapper.writeValueAsString(groupDto);
+
+        final String obfuscatedId = Obfuscator.obfuscate(group().getId().getId());
+
+        final String urlSegment = "/se/central/cluster1";
+        final ResultActions result = mvc.perform(put(BASE_PATH + "/clusteredit" + urlSegment + "/" + obfuscatedId)
+                                                         .contentType(MediaTypes.APPLICATION_JSON_V1)
+                                                         .content(json))
+                                        .andExpect(status().isOk());
+
+        final String locationHeader = result.andReturn().getResponse().getHeader("Location");
+
+        assertThat(locationHeader).isNotBlank();
+        assertThat(locationHeader).endsWith(BASE_PATH + "/clusterview" + urlSegment);
+    }
+
+    @Test
+    @WithMockCustomUser(username = "testU", password = "NotSaying")
+    public void should_delete_group() throws Exception {
+
+        final String obfuscatedId = Obfuscator.obfuscate(group().getId().getId());
+
+        final String urlSegment = "/se/central/cluster1";
+        final ResultActions result = mvc.perform(delete(BASE_PATH + "/clusteredit" + urlSegment + "/" + obfuscatedId))
+                                        .andExpect(status().isOk());
+
+        final String locationHeader = result.andReturn().getResponse().getHeader("Location");
+
+        assertThat(locationHeader).isNotBlank();
+        assertThat(locationHeader).endsWith(BASE_PATH + "/clusterview" + urlSegment);
+    }
+}
