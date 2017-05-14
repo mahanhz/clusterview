0d6f1775a7b7abc05b5c72c0564daf533c80ca0a
Mahan Hashemizadeh <mahanhz@gmail.com>
2017-05-14 18:26:59 +0200

    Fixed integration tests

--- integration-test/src/test/java/org/amhzing/clusterview/integrationtest/web/controller/user/UserRestControllerTest.java
+++ integration-test/src/test/java/org/amhzing/clusterview/integrationtest/web/controller/user/UserRestControllerTest.java
@@ -2,12 +2,13 @@ package org.amhzing.clusterview.integrationtest.web.controller.user;
 
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
-import org.amhzing.clusterview.core.boundary.enter.UserService;
-import org.amhzing.clusterview.web.controller.user.UserRestController;
+import org.amhzing.clusterview.adapter.web.UserAdapter;
+import org.amhzing.clusterview.adapter.web.util.UserDtoFactory;
 import org.amhzing.clusterview.core.domain.user.Page;
 import org.amhzing.clusterview.core.domain.user.User;
 import org.amhzing.clusterview.integrationtest.annotation.TestOffline;
 import org.amhzing.clusterview.integrationtest.security.WithMockCustomUser;
+import org.amhzing.clusterview.web.controller.user.UserRestController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.amhzing.clusterview.integrationtest.helper.RestHelper.parseJson;
 import static org.amhzing.clusterview.integrationtest.helper.UserDomainModelHelper.users;
 import static org.amhzing.clusterview.web.controller.RestControllerPath.BASE_PATH;
 import static org.amhzing.clusterview.web.controller.RestControllerPath.MANAGE_PATH;
 import static org.amhzing.clusterview.web.controller.appnav.CommonLinks.REL_HOME;
 import static org.amhzing.clusterview.web.controller.appnav.CommonLinks.USERS_PAGE_PREFIX;
-import static org.amhzing.clusterview.integrationtest.helper.RestHelper.parseJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.hateoas.Link.REL_SELF;
@@ -35,7 +36,7 @@ public class UserRestControllerTest {
     private MockMvc mvc;
 
     @MockBean
-    private UserService userService;
+    private UserAdapter userAdapter;
 
     @Test
     @WithMockCustomUser(username = "testU", password = "NotSaying")
@@ -43,7 +44,8 @@ public class UserRestControllerTest {
 
         final int urlPageNumber = 1;
 
-        given(userService.users(urlPageNumber - 1)).willReturn(pagedUsers());
+        given(userAdapter.users(urlPageNumber)).willReturn(UserDtoFactory.users((pagedUsers().getContent())));
+        given(userAdapter.pagedUsers(urlPageNumber)).willReturn(pagedUsers());
 
         final Object document = parseJson(mvc, MANAGE_PATH + BASE_PATH + "/users/page/" + urlPageNumber);
 
